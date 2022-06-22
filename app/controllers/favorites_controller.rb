570c8fc149683e29f9931f501c0c21df63ad5228
nope0124 <kami-ryo0124@ezweb.ne.jp>
2022-06-22 22:31:33 +0000

    :sparkles: いいね機能を非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,18 @@
 class FavoritesController < ApplicationController
+
   def create
+    @book = Book.find(params[:book_id])
     favorite = Favorite.new
     favorite.book_id = params[:book_id]
     favorite.user_id = current_user.id
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: params[:book_id])
-    favorite.book_id = params[:book_id]
     favorite.user_id = current_user.id
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
+
 end
